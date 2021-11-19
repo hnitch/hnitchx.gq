a57cdedc6ddf23e65919c2c9a9d3c1c3b1ac0263
hnitch <hnitchx@gmail.com>
2021-11-19 10:10:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+document.onkeydown = function(e) {
+if(event.keyCode == 123) {
+return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+return false;
+}
+}
+
 document.querySelectorAll("details[open]").forEach(d => d.removeAttribute("open"));
 var profilePicture = document.getElementById("profilePicture");
 
